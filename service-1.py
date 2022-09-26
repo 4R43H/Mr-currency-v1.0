d27b3f57171c544bdbb2355d0330046b1f936a8e
Mr CODER <93862526+IamVeryAlone@users.noreply.github.com>
2022-09-26 20:55:17 +0330

    Update service-1.py

--- service-1.py
+++ service-1.py
@@ -7,21 +7,43 @@ Update in --> 13 SEP 2022
 support time --> forever :)
 Please Email:((((( --> coderpy@yahoo.com 
 """
+
+"""
+In This section we import needs to run program!
+
+"""
+
 import sys
 import requests as req
 from bs4 import BeautifulSoup as bs
 import os
 
+"""
+In here I define functions and this is main function and all of program will run in this funtion.
+"""
 def pro():
+    """
+    In This section I define my banner for program !
+    """
     try:
         os.system("clear")
         print("Welcom to X service")
         print("you can follow up the price of bitcoin from this service")
+        print("This service working with sms and send me the message.")
+        print("Tnx for using the Mr-Currency and X-service ...")
 
+        
+        """
+        In this section we define inputs and I get values of them from user.
+        """
         inp = input("if you want to follow up enter your phone number(only iran WHIT 0): ")
         name = input("Enter your name: ")
         my_good_price = int(input("Enter your price to follow up the bitcoin price: "))
 
+
+        """
+        This main function and I define it for requests to website and get the information from website.
+        """
         def main():
             while True:
                 r = req.get("https://www.coindesk.com/price/bitcoin")
@@ -33,6 +55,10 @@ def pro():
                     res = val1.text.replace(",", "")
                     inform_to_user()
 
+        """
+        I define this section for inform to user. if price of bitcoin was smaller than my good price, This section inform to me
+        and the main usage of program it's inform_to_user function!
+        """
         def inform_to_user():
             while True:
                 api_key = "6E4572424D6A327852304631497966516E455749556D334C796E716C7646734147326452425463514975553D"
@@ -40,17 +66,26 @@ def pro():
                 payload = {"receptor":inp, "message":"Hi {} ... price is good!".format(name)}
                 t = req.post(url, data=payload)
                 check()
-
+        """
+        This function check the price for requests and sms to me!
+        """
         def check():
             while True:
                 if float(res) <= float(my_good_price):
                     inform_to_user()
-            
+        """
+        I call my function to start the program
+        """           
         main()
-
+        """
+        if user enter CTRL-C program return this text and exit.
+        """
     except KeyboardInterrupt:
         print("\nBad input!")
         sys.exit()
 
+"""
+This is final run.
+"""
 if __name__ == "__main__":
     pro()
